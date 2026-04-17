180da11e00d930d1887eb43d70f3ff0dfafd9396
Thomagotchi <thomassano87@gmail.com>
2026-04-17 09:59:37 +0200

    Update - Service - UserInfo - Changed port to 3000

--- src/services/userInfo.jsx
+++ src/services/userInfo.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export async function getUserPerformance({ id }) {
   try {
     const response = await axios.get(
-      `http://localhost:3001/user/${id}/performance`,
+      `http://localhost:3000/user/${id}/performance`,
     );
     return response.data;
   } catch (error) {
@@ -15,7 +15,7 @@ export async function getUserPerformance({ id }) {
 export async function getUserActivity({ id }) {
   try {
     const response = await axios.get(
-      `http://localhost:3001/user/${id}/activity`,
+      `http://localhost:3000/user/${id}/activity`,
     );
     return response.data;
   } catch (error) {
@@ -27,7 +27,7 @@ export async function getUserActivity({ id }) {
 export async function getUserAverage({ id }) {
   try {
     const response = await axios.get(
-      `http://localhost:3001/user/${id}/average-sessions`,
+      `http://localhost:3000/user/${id}/average-sessions`,
     );
     return response.data;
   } catch (error) {
@@ -38,7 +38,7 @@ export async function getUserAverage({ id }) {
 
 export async function getUser({ id }) {
   try {
-    const response = await axios.get(`http://localhost:3001/user/${id}`);
+    const response = await axios.get(`http://localhost:3000/user/${id}`);
     return response.data;
   } catch (error) {
     console.error(error);
