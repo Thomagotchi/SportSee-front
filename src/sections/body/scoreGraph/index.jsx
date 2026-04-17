c0e57acdfc3192701bc6094ceb2b97bf12f54941
Thomagotchi <thomassano87@gmail.com>
2026-04-17 09:58:48 +0200

    Update - Score Graph - Seperated SCSS

--- src/sections/body/scoreGraph/index.jsx
+++ src/sections/body/scoreGraph/index.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { PieChart, Pie, Cell } from "recharts";
 import { getUser } from "../../../services/userInfo";
 import { userScoreTransformer } from "../../../utils/transformers/userInfoTransformer";
+import styles from "./styles.module.scss";
 
 const USER_ID = 12;
 
@@ -17,54 +18,16 @@ const ScoreGraphSection = () => {
   const chartData = [{ value: score }, { value: 100 - score }];
 
   return (
-    <div
-      style={{
-        background: "#FBFBFB",
-        borderRadius: 5,
-        padding: "24px",
-        position: "relative",
-      }}
-    >
-      <h2
-        style={{
-          fontSize: 15,
-          fontWeight: 500,
-          color: "#282D30",
-          margin: 0,
-        }}
-      >
-        Score
-      </h2>
-
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          position: "relative",
-        }}
-      >
-        <PieChart width={200} height={200}>
-          {/* Background track */}
-          <Pie
-            data={[{ value: 1 }]}
-            cx="50%"
-            cy="50%"
-            innerRadius={68}
-            outerRadius={80}
-            startAngle={0}
-            endAngle={360}
-            dataKey="value"
-            strokeWidth={0}
-            fill="#EDEDED"
-          />
-          {/* Score arc */}
+    <div className={styles.score_graph_wrap}>
+      <h2 className={styles.score_graph_title}>Score</h2>
+      <div className={styles.radar_graph_content}>
+        <PieChart width="90%" height="90%">
           <Pie
             data={chartData}
             cx="50%"
             cy="50%"
-            innerRadius={68}
-            outerRadius={80}
+            innerRadius={58}
+            outerRadius={70}
             startAngle={90}
             endAngle={90 + 360}
             dataKey="value"
@@ -75,44 +38,9 @@ const ScoreGraphSection = () => {
             <Cell fill="transparent" />
           </Pie>
         </PieChart>
-
-        {/* Inner white circle + center text */}
-        <div
-          style={{
-            position: "absolute",
-            width: 136,
-            height: 136,
-            borderRadius: "50%",
-            background: "#FBFBFB",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            textAlign: "center",
-          }}
-        >
-          <span
-            style={{
-              fontSize: 26,
-              fontWeight: 700,
-              color: "#282D30",
-              lineHeight: 1,
-            }}
-          >
-            {score}%
-          </span>
-          <span
-            style={{
-              fontSize: 16,
-              fontWeight: 500,
-              color: "#74798C",
-              maxWidth: 90,
-              lineHeight: 1.4,
-              marginTop: 8,
-            }}
-          >
-            de votre objectif
-          </span>
+        <div className={styles.radar_graph_text}>
+          <span className={styles.radar_graph_title}>{score}%</span>
+          <span className={styles.radar_graph_subtitle}>de votre objectif</span>
         </div>
       </div>
     </div>
